f2c40c037d0256ca5cb1650283f05d882b926f37
Gaxsun <Razgriz248@gmail.com>
2017-08-24 10:45:48 +1000

    Teleporting prototype. Still intend to use 1 script for teleporting as well as overhaul the movement

---
+++ J.A.N.U.S Prototype/Assets/scripts/teleportToGreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleportToGreen : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        player.transform.position = new Vector3(20, -8, 1);
+    }
+}
