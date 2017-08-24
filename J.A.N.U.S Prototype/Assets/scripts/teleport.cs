bb2ec032076abff5baaf95f2f1ad78766faf2f38
Gaxsun <Razgriz248@gmail.com>
2017-08-24 12:02:03 +1000

    Universal teleport script works, simply plug in the subject to be teleported and target objject to teleport to (any game object even and empty one) and the subject will teleport straight to the location.

---
+++ J.A.N.U.S Prototype/Assets/scripts/teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour {
+
+    public GameObject tpSubject;
+    public GameObject tpTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        tpSubject.transform.position = tpTarget.transform.position;
+    }
+
+}
